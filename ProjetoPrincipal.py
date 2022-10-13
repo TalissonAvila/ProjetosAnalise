1d33935dfcdc1e84fa17d32beb11117952b2b50f
Herik <hherik96@gmail.com>
2022-10-13 15:03:13 -0300

    Atualizando-de-int-para-float

--- ProjetoPrincipal.py
+++ ProjetoPrincipal.py
@@ -1,6 +1,6 @@
 import random
-from InsertionSort import insertionSort
-from mergeSort import mergeSort
+from insertionsort import insertionsort
+from mergesort import mergesort
 import time
 
 n = []
@@ -21,34 +21,34 @@ for i in range(10):
     n.append(random.randint(10, 10001))
 
 for i in range(n[0]):
-    vetor1.append(random.randint(n[0]*-2, n[0]*2 + 1))
+    vetor1.append(random.uniform(n[0]*-2, n[0]*2 + 1))
 
 for i in range(n[1]):
-    vetor2.append(random.randint(n[1]*-2, n[1]*2 + 1))
+    vetor2.append(random.uniform(n[1]*-2, n[1]*2 + 1))
 
 for i in range(n[2]):
-    vetor3.append(random.randint(n[2]*-2, n[2]*2 + 1))
+    vetor3.append(random.uniform(n[2]*-2, n[2]*2 + 1))
 
 for i in range(n[3]):
-    vetor4.append(random.randint(n[3]*-2, n[3]*2 + 1))
+    vetor4.append(random.uniform(n[3]*-2, n[3]*2 + 1))
 
 for i in range(n[4]):
-    vetor5.append(random.randint(n[4]*-2, n[4]*2 + 1))
+    vetor5.append(random.uniform(n[4]*-2, n[4]*2 + 1))
 
 for i in range(n[5]):
-    vetor6.append(random.randint(n[5]*-2, n[5]*2 + 1))
+    vetor6.append(random.uniform(n[5]*-2, n[5]*2 + 1))
 
 for i in range(n[6]):
-    vetor7.append(random.randint(n[6]*-2, n[6]*2 + 1))
+    vetor7.append(random.uniform(n[6]*-2, n[6]*2 + 1))
 
 for i in range(n[7]):
-    vetor8.append(random.randint(n[7]*-2, n[7]*2 + 1))
+    vetor8.append(random.uniform(n[7]*-2, n[7]*2 + 1))
 
 for i in range(n[8]):
-    vetor9.append(random.randint(n[8]*-2, n[8]*2 + 1))
+    vetor9.append(random.uniform(n[8]*-2, n[8]*2 + 1))
 
 for i in range(n[9]):
-    vetor10.append(random.randint(n[9]*-2, n[9]*2 + 1))
+    vetor10.append(random.uniform(n[9]*-2, n[9]*2 + 1))
 
 
 def tempo(vetor):
@@ -58,12 +58,12 @@ def tempo(vetor):
     somab = 0
     for i in range(tamanho_random):
         inicial = time.time()
-        insertionSort(vetor)
+        insertionsort(vetor)
         final = time.time()
         a.append(final-inicial)
 
         inicial = time.time()
-        mergeSort(vetor, 0, len(vetor) -1)
+        mergesort(vetor, 0, len(vetor) -1)
         final = time.time()
         b.append(final - inicial)
 
@@ -77,9 +77,10 @@ def tempo(vetor):
     print(f'Tamanho do vetor: {len(vetor)}\n'
     f'Media Insertion Sort: {media}')
     print('-'*30)
-    print(f'Tamanho do vetor: {len(vetor)}\n'
-    f'Media Merge Sort: {mediab}')
+    print(f'Media Merge Sort: {mediab}')
 
+
+print('-'*30)
 tempo(vetor1)
 print('-'*30)
 tempo(vetor2)
